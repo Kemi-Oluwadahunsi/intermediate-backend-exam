783b62178c743e9333abb1479c7214980a8b7b17
TechMrs <kemilat50@gmail.com>
2024-05-17 23:16:52 +0800

    Updated some functions

--- controllers/blog.js
+++ controllers/blog.js
@@ -65,8 +65,188 @@ const aPublishedBlog = async (req, res) => {
   }
 };
 
+const getAllBlogs = async (req, res) => {
+  try {
+    let { page, limit, author, title, tags, sortBy, sortDirection } = req.query;
+    page = parseInt(page) || 1;
+    limit = parseInt(limit) || 20;
+    const skip = (page - 1) * limit;
+
+    let query = { state: "published" };
+
+    if (author) {
+      const authors = await User.find({
+        $or: [
+          { first_name: { $regex: author, $options: "i" } },
+          { last_name: { $regex: author, $options: "i" } },
+        ],
+      });
+
+      const authorIds = authors.map((author) => author._id);
+
+      query.author = { $in: authorIds };
+    }
+
+    if (title) query.title = { $regex: title, $options: "i" };
+    if (tags) query.tags = { $in: tags.split(",") };
+
+    const sortOptions = {};
+    if (
+      sortBy &&
+      ["read_count", "reading_time", "createdAt", "updatedAt"].includes(sortBy)
+    ) {
+      sortOptions[sortBy] = sortDirection === "desc" ? -1 : 1;
+    } else {
+      sortOptions.createdAt = -1; // Default sorting by createdAt in descending order
+    }
+
+    const blogs = await Blog.find(query)
+      .sort(sortOptions)
+      .skip(skip)
+      .limit(limit)
+      .populate("author", "first_name last_name");
+
+    if (blogs.length === 0) {
+      return res
+        .status(200)
+        .json({ message: "No blog posts yet!", data: blogs });
+    }
+
+    res.status(200).json({ message: "Blogs List", data: blogs });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: error.message, message: "Internal server error" });
+  }
+};
+
+
+const updateBlog = async (req, res) => {
+  const userId = req.user.id;
+  const blogId = req.params.id;
+  const { title, description, tags, body } = req.body;
+  try {
+    const blog = await Blog.findById(blogId);
+    if (!blog) {
+      return res.status(404).json({ error: "Blog not found" });
+    }
+
+    if (blog.author.toString() !== userId) {
+      return res.status(403).json({ error: "You are not authorized to update this blog" });
+    }
+
+    if (title) {
+      const existingTitle = await Blog.findOne({ title, _id: { $ne: blogId } });
+      if (existingTitle) {
+        return res
+          .status(400)
+          .json({ error: "Title is not available: Try another one!" });
+      }
+      blog.title = title;
+    }
+
+    if (description) blog.description = description;
+    if (tags) blog.tags = tags;
+    if (body) {
+      blog.body = body;
+      blog.reading_time = calculateReadingTime(body);
+    }
+
+    await blog.save();
+    res.status(200).json({ message: "Blog updated successfully", data: blog });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: error.message, message: "Internal server error" });
+  }
+};
+
+const updateBlogState = async (req, res) => {
+  const userId = req.user.id;
+  const blogId = req.params.id;
+  const { state } = req.body;
+  try {
+    const blog = await Blog.findById(blogId);
+    if (!blog) {
+      return res.status(404).json({ error: "Blog not found" });
+    }
+
+    if (blog.author.toString() !== userId) {
+      return res.status(403).json({ error: "You are not authorized to update this blog" });
+    }
+
+    if (state && ["draft", "published"].includes(state)) {
+      blog.state = state;
+    } else {
+      return res.status(400).json({ error: "Invalid state value" });
+    }
+
+    await blog.save();
+    res.status(200).json({ message: "Blog state updated successfully", data: blog });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: error.message, message: "Internal server error" });
+  }
+};
+
+const deleteBlog = async (req, res) => {
+  const userId = req.user.id;
+  const blogId = req.params.id;
+  try {
+    const blog = await Blog.findById(blogId);
+    if (!blog) {
+      return res.status(404).json({ error: "Blog not found" });
+    }
+
+    if (blog.author.toString() !== userId) {
+      return res.status(403).json({ error: "You are not authorized to delete this blog" });
+    }
+
+    await blog.remove();
+    res.status(200).json({ message: "Blog deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message, message: "Internal server error" });
+  }
+};
+
+export const getUserBlogs = async (req, res) => {
+  const userId = req.user.id;
+
+  const { page = 1, limit = 10, state } = req.query;
+  try {
+    let filter = { author: userId };
+    if (state) {
+      filter.state = state;
+    }
+
+    const paginationOptions = {
+      limit: parseInt(limit),
+      skip: (parseInt(page) - 1) * parseInt(limit),
+    };
+    const blogs = await Blog.find(filter, null, paginationOptions);
+
+    const totalBlogs = await Blog.find(filter).count();
+    res.status(200).json({
+      message: "All available blogs",
+      data: blogs,
+      totalPages: Math.ceil(totalBlogs / parseInt(limit)),
+      currentPage: parseInt(page),
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: error.message, message: "Internal server error" });
+  }
+};
+
 module.exports = {
   createBlog,
   allPublishedBlog,
   aPublishedBlog,
+  getAllBlogs,
+  updateBlog,
+  updateBlogState,
+  deleteBlog,
+  getUserBlogs,
 };
--- routes/blog.js
+++ routes/blog.js
@@ -6,14 +6,20 @@ const {
   createBlog,
   allPublishedBlog,
   aPublishedBlog,
+  getAllBlogs,
+  updateBlog,
+  updateBlogState,
+  deleteBlog,
+  getUserBlogs,
 } = require("../controllers/blog");
 
 router.route("/create").post(requireSignIn, createBlog);
+router.route("/getAll").get(requireSignIn, getAllBlogs);
 router.route("/all").get(allPublishedBlog);
 router.route("/:id").get(aPublishedBlog);
-// router.route("/:id").put(updateState);
-// router.route("/:id").delete(deleteBlog);
-// router.route("/:id").put(updateBlog);
-// router.route("/owner").get(getOwnerBlog);
+router.route("/:id").put(requireSignIn,  updateBlog);
+router.route("/:id/state").patch(requireSignIn, updateBlogState);
+router.route("/:id").delete(requireSignIn, deleteBlog);
+router.route("/owner").get(requireSignIn, getUserBlogs);
 
 module.exports = router;
